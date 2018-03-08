e38b1b72bfd942e892087b541eff453c0fd886cf
KSHIBA-HHQ <33481625+KSHIBA-HHQ@users.noreply.github.com>
2018-03-08 21:26:38 +0900

    Update kalman c++(lesson6).cpp

--- kalman c++(lesson6).cpp
+++ kalman c++(lesson6).cpp
@@ -133,7 +133,9 @@ void filter(VectorXd &x, MatrixXd &P) {
 
         // prediction
         x=(F*x)+u;
-        P=F*P*F.transpose();
+		
+        //P=F*P*F.transpose();
+        P=F*P*F.transpose()+Q;
         
 		
 		std::cout << "x=" << std::endl <<  x << std::endl;
